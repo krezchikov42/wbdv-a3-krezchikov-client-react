ccc877e336db2da965a295489385e138ac42a71e
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-09-29 19:18:06 -0400

    Initial structure of course service file

---
+++ src/services/CourseService.js
@@ -0,0 +1,30 @@
+class CourseService() {
+
+    //creates a new course instance and adds it to the collection of courses
+    createCourse(course) {
+
+    }
+
+    //retrieves all course instances as an array of courses
+    findAllCourses() {
+
+    }
+
+    //retrieves a course instance that matches the id parameter
+    findCourseById(id) {
+
+    }
+
+    // updates the course instance whose id matches the id parameter.Updates the instance with values in course parameter
+    updateCourse(id, course) {
+
+    }
+
+    // deletes course instance whose id matches the id parameter
+    deleteCourse(id) {
+
+    }
+
+
+
+}
