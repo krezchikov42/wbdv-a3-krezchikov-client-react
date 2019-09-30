90e62ce3496fcba43a4483709a8195917e3d19d9
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-09-29 22:02:12 -0400

    create file for Course Manager Screen

---
+++ src/components/CourseManager.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Container from 'react-bootstrap/Container'
+
+
+
+export default class CourseManger extends React.Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+
