d5c86c36bee22ccb57b7b805d57ff77ae486a126
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-10-01 13:53:47 -0400

    implement methods in Course Service

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,28 +1,47 @@
-class CourseService() {
+export default class CourseService {
+
+    static myInstance = null;
+
+
+
+    static getInstance() {
+        if (CourseService.myInstance == null) {
+            CourseService.myInstance = new CourseService();
+        }
+
+        return this.myInstance;
+    }
+
+    constructor() {
+        this.courses = require('./courses.json')
+
+    }
 
     //creates a new course instance and adds it to the collection of courses
     createCourse(course) {
-
+        this.courses.push(course)
     }
 
     //retrieves all course instances as an array of courses
     findAllCourses() {
-
+        return this.courses;
     }
 
     //retrieves a course instance that matches the id parameter
     findCourseById(id) {
-
+        found_course = this.courses.find(course => id == course.id);
+        return found_course
     }
 
     // updates the course instance whose id matches the id parameter.Updates the instance with values in course parameter
     updateCourse(id, course) {
-
+        found_course_index = this.courses.indIndex(course => id == course.id);
+        this.courses[found_course_index] = course;
     }
 
     // deletes course instance whose id matches the id parameter
     deleteCourse(id) {
-
+        this.courses = this.courses.filter(course => course.id !== id);
     }
 
 
