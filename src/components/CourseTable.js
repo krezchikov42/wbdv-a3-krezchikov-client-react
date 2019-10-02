30fe6e8acbc68734473d9cef9b82b6fb9bd78a9b
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-10-02 13:50:55 -0400

    adding className to CourseTable

--- src/components/CourseTable.js
+++ src/components/CourseTable.js
@@ -8,21 +8,21 @@ export default class CourseTable extends React.Component {
 
     render() { 
         let list_rows = this.props.course_many.map(course_one => <CourseRow course={course_one} />)
-        return (<table class="table table-primary table-hover">
-                <thead class="bg-light">
-                    <tr class=" d-none d-sm-table-row">
-                        <th>Class Name</th>
+        return (<table className="table table-primary table-hover">
+                <thead className="bg-light">
+                    <tr className=" d-none d-sm-table-row">
+                        <th>className Name</th>
                         <th>Owner</th>
                         <th>Last Modified</th>
                         <th>
-                            <button type="button" class="btn btn-default wbdv-button wbdv-grid-layout" aria-label="Left Align">
-                                        <span class="glyphicon glyphicon-th" aria-hidden="true"></span>
+                            <button type="button" className="btn btn-default wbdv-button wbdv-grid-layout" aria-label="Left Align">
+                                        <span className="glyphicon glyphicon-th" aria-hidden="true"></span>
                                     </button>
-                            <button type="button" class="btn btn-default wbdv-button wbdv-list-layout" aria-label="Left Align">
-                                            <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
+                            <button type="button" className="btn btn-default wbdv-button wbdv-list-layout" aria-label="Left Align">
+                                            <span className="glyphicon glyphicon-th-list" aria-hidden="true"></span>
                                         </button>
-                            <button type="button" class="btn btn-default wbdv-header wbdv-sort" aria-label="Left Align">
-                                            <span class="glyphicon glyphicon-font" aria-hidden="true"></span>
+                            <button type="button" className="btn btn-default wbdv-header wbdv-sort" aria-label="Left Align">
+                                            <span className="glyphicon glyphicon-font" aria-hidden="true"></span>
                                     </button>
                         </th>
                     </tr>
