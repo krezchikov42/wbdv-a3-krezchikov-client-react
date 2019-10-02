1b058a2b1405af87d0cbda6402cdbd1549447c8a
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-10-02 14:24:30 -0400

    don't use react-bootstrap anymore because it's pretty bad

--- src/components/CourseManager.js
+++ src/components/CourseManager.js
@@ -1,45 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Navbar from "react-bootstrap/Navbar";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import FormControl from "react-bootstrap/FormControl";
 import CourseTable from "./CourseTable";
 import CourseService from "../services/CourseService";
-import CourseHeader from "./CourseHeader"
+import CourseHeader from "./CourseHeader";
 
 export default class CourseManger extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-        'course_many': []
-      }
+      course_many: []
+    };
   }
 
   componentDidMount() {
     let course_service = CourseService.getInstance();
     let course_many = course_service.findAllCourses();
-    this.setState({'course_many': course_many})
+    this.setState({ course_many: course_many });
 
   }
 
   render() {
     return (
-      <Container>
-        <Row>
-          <Col>
+      <div className="container-fluid">
+        <div className="row bg-dark">
+          <div className="col">
             <CourseHeader />
-          </Col>
-        </Row>
-        <Row>
-          <CourseTable 
-            course_many = {this.state.course_many}/>
-        </Row>
-      </Container>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-1" />
+          <div className="col-10">
+            <CourseTable course_many={this.state.course_many} />
+          </div>
+          <div className="col-1" />
+        </div>
+      </div>
     );
   }
 }
