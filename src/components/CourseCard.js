8baca84690dbbb5a4cea4929696eb84cc3b46c7f
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-10-03 14:25:17 -0400

    adding removing classes to Card Grid

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React from "react";
+import CourseService from "../services/CourseService"
 
-export default class CourseCard extends React.Component
-{
-    render() {
-        return (
-            <div className="card" style={{width: '18rem',}}>
-                <img className="card-img-top" style={{width: '50px', height: '50px'}}
-                     src="https://picsum.photos/300/200"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Card text.</p>
-                    <a href="#" className="btn btn-primary">More...</a>
-                </div>
-            </div>
-        )
-    }
+export default class CourseCard extends React.Component {
+  render() {
+      let course = this.props.course
+    return (
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          style={{ width: "50px", height: "50px" }}
+          src="https://picsum.photos/300/200"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{course.title}</h5>
+          <p className="card-text">Card text.</p>
+          <button
+            type="button"
+            className="btn btn-outline-light wbdv-row wbdv-button wbdv-delete"
+            aria-label="Remove Course"
+            onClick={this.removeClass}
+          >
+            <span
+              className="glyphicon glyphicon-remove"
+              aria-hidden="true"
+            ></span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  removeClass = e => {
+    let course_service = CourseService.getInstance();
+    let course = this.props.course;
+    course_service.deleteCourse(course.id);
+    this.props.renderCourses();
+  };
 }
--- src/components/CourseGrid/CourseGrid.js
+++ src/components/CourseGrid/CourseGrid.js
@@ -5,7 +5,7 @@ import "./CourseGridStyle.css";
 export default class CourseGrid extends React.Component {
   render() {
     let course_card_many = this.props.course_many.map(course => (
-      <CourseCard course={course} key={course.id} />
+      <CourseCard course={course} key={course.id} renderCourses={this.props.renderCourses}/>
     ));
     return <div className="card-columns">{course_card_many}</div>;
   }
