735d962d43cc932ced55551e4ba76f1f38ba274d
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-10-04 09:52:07 -0400

    add header to course grid

--- src/components/CourseGrid/CourseGrid.js
+++ src/components/CourseGrid/CourseGrid.js
@@ -1,12 +1,22 @@
 import React from "react";
 import CourseCard from "../CourseCard";
+import CourseGridHeader from "./CourseGridHeader"
 import "./CourseGridStyle.css";
 
 export default class CourseGrid extends React.Component {
   render() {
     let course_card_many = this.props.course_many.map(course => (
-      <CourseCard course={course} key={course.id} renderCourses={this.props.renderCourses}/>
+      <CourseCard
+        course={course}
+        key={course.id}
+        renderCourses={this.props.renderCourses}
+      />
     ));
-    return <div className="card-columns">{course_card_many}</div>;
+    return (
+      <div>
+        <CourseGridHeader />
+        <div className="card-columns">{course_card_many}</div>
+      </div>
+    );
   }
 }
