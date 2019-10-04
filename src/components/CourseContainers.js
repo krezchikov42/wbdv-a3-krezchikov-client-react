36ced8d1262e48cc9ffd13b5a304f236ea91b054
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-10-04 10:07:07 -0400

    adding template for Course Container

---
+++ src/components/CourseContainers.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CourseGrid from './CourseGrid/CourseGrid'
+import CourseTable from './CourseTable'
+
+const CourseStyles = {
+    GRID: 'grid',
+    Table: 'table'
+}
+
+
+class CourseContainer extends React.Component {
+    render() {
+
+    }
+}
