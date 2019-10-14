d0aed5355b985d78e30f086e8f2bcfd2a6a34f0c
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-10-13 20:29:00 -0400

    create template for widget service

---
+++ src/services/WidgetService.js
@@ -0,0 +1,28 @@
+export default class WidgetService {
+
+    //creates a new widget instance for the topic whose ID is topicId
+    createWidget(topicId, widget) {
+
+    }
+
+    //retrieves all widgets for topic whose ID is topicId
+    findWidgets(topicId) {
+
+    }
+
+    //retrieves one widget object whose ID is widgetId
+    findWidget(widgetId) {
+
+    }
+
+    //updates one widget object whose ID is widgetId
+    updateWidget(widgetId, widget) {
+
+    }
+
+    //removes widget whose ID is widgetId
+    deleteWidget(widgetId) {
+
+    }
+
+}
