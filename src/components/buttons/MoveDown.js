02864e2b23e609a753906117f4f5aafecc6c33fa
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-10-16 18:22:47 -0400

    add move down button

---
+++ src/components/buttons/MoveDown.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class MoveDownButton extends React.Component {
+  render() {
+    return (<div>
+        {this.props.should_render && (
+            <button
+              type="button"
+              className="btn btn-primary"
+              aria-label="Remove Course"
+              onClick={() => this.props.onClick(this.props.index, this.props.index +1)}
+            >
+              <span className="glyphicon glyphicon glyphicon-arrow-up" aria-hidden="true"></span>
+            </button>)}
+    </div>);
+  }
+}
