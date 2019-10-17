37e07de98d9a24f2b2cd969a3b658e7a77ce3742
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-10-17 12:56:57 -0400

    changing name of delete button

--- src/components/buttons/DeleteButton.js
+++ src/components/buttons/DeleteButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default class EditButton extends React.Component {
+export default class DeleteButton extends React.Component {
   render() {
     return (
       <button
         type="button"
-        className="btn btn-primary wbdv-row wbdv-button wbdv-deletemx-2"
+        className={"btn btn-primary wbdv-row wbdv-button wbdv-deletemx-2" +this.props.className}
         aria-label="Remove Course"
         onClick={() => this.props.delete(this.props.item.id)}
       >
